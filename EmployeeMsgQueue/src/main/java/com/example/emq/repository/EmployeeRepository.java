c7fac1da37b227e571bf9291b5c46dadcc494484
alycia <mokeiraalice4@gmail.com>
2025-01-16 14:05:06 +0300

    first commit

--- EmployeeMsgQueue/src/main/java/com/example/emq/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.emq.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.emq.entity.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long>{
-
-}
